6d64134916742a257675b4058943ac55d224eeb9
Kate Telfer <katherinetelfer652@gmail.com>
2025-03-23 14:46:43 +0000

    fjffjfjjffjjfjjfjfjffjjf

--- possibleMainPlan1.R
+++ possibleMainPlan1.R
@@ -36,58 +36,6 @@ import_eBird <- function(eBird_infile, eBird_outfile) {
   selected <- auk_select(ebd, select=eBird_fields, file=eBird_outfile)
 }
 
-transform_eBird_by_month <- function(eBird_infile, eBird_outfile_root) {
-  #added to docs
-  # Reduce the large number of fields in the eBird dataset to a minimal set
-  # resulting in reduced RAM usage.
-  # Write data into yearly and monthly units to make manageable chunks.
-  eBird_fields <- c(
-    "GROUP IDENTIFIER",           # Required
-    "SAMPLING EVENT IDENTIFIER",  # Required
-    "SCIENTIFIC NAME",            # Required
-    "OBSERVATION COUNT",
-    "COUNTRY",
-    "STATE",
-    "LOCALITY",
-    "LATITUDE",
-    "LONGITUDE",
-    "OBSERVATION DATE")
-  month_days = c(31,28,31,30,31,30,31,31,30,31,30,31)
-  
-  print(paste("Loading",eBird_infile))
-  for (year in 2014:2024) {
-    for (month in 1:12) {
-      eBird_outfile <- paste(eBird_outfile_root,year,month,sep="-")
-      print(paste("Writing to",eBird_outfile))
-      ebd <- auk_ebd(eBird_infile)
-      filters <- auk_year(ebd, year)
-      filters <- auk_date(ebd, 
-                          date = c(paste(year,month,"1",sep="-"), 
-                                   paste(year,month,month_days[month],sep="-")))
-      filtered <- auk_filter(filters, file = eBird_outfile, overwrite=TRUE)
-    }
-  }
-}
-
-load_eBird_simplified <- function(eBird_infile) {
-  eBird_field_formats <- c(
-    "character", # SCIENTIFIC NAME
-    "character", # OBSERVATION COUNT
-    "character", # COUNTRY
-    "character", # STATE
-    "character", # LOCALITY
-    "numeric",   # LATITUDE
-    "numeric",   # LONGITUDE
-    "Date",      # OBSERVATION DATE
-    "character", # SAMPLING EVENT IDENTIFIER
-    "NULL"       # GROUP IDENTIFIER
-  )
-  eBird_data <- read.table(eBird_infile, header=TRUE, sep="\t", quote="",
-                           #added to the file
-                           colClasses=eBird_field_formats, 
-                           comment.char="", fill=TRUE)
-  return(eBird_data)
-}
 
 load_eBird <- function(eBird_infile) {
   eBird_field_formats <- c(
@@ -147,41 +95,6 @@ load_eBird <- function(eBird_infile) {
   return(eBird_data)
 }
 
-eBird_accumulate <- function(eBird_data) {
-  # Generate a histogram for all latitudes and longitudes
-  
-  # Create an empty array of all latitudes and longitudes for accumulation
-  # Array subscript is shifted by (-90,-180) from (latitude,longitude) values
-  world_grid <- array(NA,c(180,360))
-  for (i in 1:nrow(eBird_data)) {
-    record <- eBird_data[i,]
-    lat <- as.integer(record$LATITUDE + 0.5 + 90.0)
-    lon <- as.integer(record$LONGITUDE + 0.5 + 180.0) 
-    if (is.na(world_grid[lat,lon])) {
-      world_grid[lat,lon] <- 1L
-    } else {
-      world_grid[lat,lon] <- world_grid[lat,lon]+1L
-    }
-  }
-  
-  # Convert array to list of coordinates and accumulated values for plotting
-  result_points <- data.frame(latitude=numeric(), longitude=numeric(), count=numeric())
-  for (grid_lat in 1:180) {
-    for (grid_long in 1:360) {
-      if (is.na(world_grid[grid_lat,grid_long])) { next }
-      grid_record <- c(grid_lat-90,grid_long-180,world_grid[grid_lat,grid_long])
-      result_points <- rbind(result_points, grid_record)
-    }
-  }
-  # The rbind appears to overwrite column names
-  colnames(result_points) <- c("latitude", "longitude", "count")
-  return(result_points)
-}
-
-eBird_data <- load_eBird("www/nowimfinallyclean-2014-6")
-print(paste("Read",nrow(eBird_data),"rows"))
-result_points <- eBird_accumulate(eBird_data)
-summary(result_points)
 
 
 #Importing the libraries used to make my application!!
